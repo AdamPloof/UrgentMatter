4150e1ba6e4565d7c38ff0222393c816d7b52b01
Adam Ploof <adamploof@hotmail.com>
2024-03-29 00:21:30 -0400

    add portal banner and flesh out the portal page in general

--- assets/avatarPicker.js
+++ assets/avatarPicker.js
@@ -3,18 +3,21 @@
  * It keeps track of the following elements on the page:
  *  - The avatar hidden form field (this is what's submitted with the form)
  *  - The avatar selections (to display after making a selection)
+ *  - The user avatar in the nav bar
  *  - The active avatar selection
  *  - The avatar options in the modal (this is what the user selects)
  */
 export default class AvatarPicker {
     inputField;
     avatarSelections;
+    userAvatars;
     activeSelection;
     avatarOptions;
 
     constructor() {
         this.inputField = document.getElementById('ticket_avatar');
         this.avatarSelections = document.querySelectorAll('.avatar-selection');
+        this.userAvatars = document.querySelectorAll('.user-avatar');
         this.avatarOptions = document.querySelectorAll('.avatar-option');
         this.activeSelection = this.findActiveSelection();
         this.initOptions();
@@ -43,16 +46,48 @@ export default class AvatarPicker {
      */
     handleSelectAvatar(avatarId) {
         this.inputField.value = `${avatarId}.jpg`;
+        this.hideAllSelections();
+        this.showAvatarSelection(avatarId);
+        // this.hideAllUserAvatars();
+        // this.showUserAvatar(avatarId);
+    }
+
+    showAvatarSelection(avatarId) {
         for (const selection of this.avatarSelections) {
             if (selection.id === `avatar_selection_${avatarId}`) {
                 selection.classList.remove('hide');
                 if (!selection.classList.contains('active')) {
                     selection.classList.add('active');
                 }
-            } else {
-                if (!selection.classList.contains('hide')) {
-                    selection.classList.add('hide');
+            }
+        }
+    }
+
+    hideAllSelections() {
+        for (const selection of this.avatarSelections) {
+            if (!selection.classList.contains('hide')) {
+                selection.classList.add('hide');
+            }
+        }
+    }
+
+    showUserAvatar(avatarId) {
+        console.log(`user_avatar_${avatarId}`);
+
+        for (const avatar of this.userAvatars) {
+            if (avatar.id === `user_avatar_${avatarId}`) {
+                avatar.classList.remove('hide');
+                if (!avatar.classList.contains('active')) {
+                    avatar.classList.add('active');
                 }
+            }
+        }
+    }
+
+    hideAllUserAvatars() {
+        for (const avatar of this.userAvatars) {
+            if (!avatar.classList.contains('hide')) {
+                avatar.classList.add('hide');
             }
         }
     }
