73b7cb3e517a215725243eb2afa27d9041bea48d
Adam Ploof <adamploof@hotmail.com>
2024-04-01 21:30:28 -0400

    use relative path for asset and API paths in js components

--- assets/includes/paths.js
+++ assets/includes/paths.js
@@ -1,6 +1,6 @@
-const origin = process.env.ORIGIN;
+// const origin = process.env.ORIGIN;
 const base = process.env.BASE_URL;
-export const FETCH_TICKET = origin + base + 'service-board/fetch';
-export const FETCH_TICKETS = origin + base + 'service-board/fetch-all';
-export const GENERATE_TICKETS = origin + base + 'service-board/generate';
-export const ICONS = origin + base + 'build/images/icons';
+export const FETCH_TICKET = base + 'service-board/fetch';
+export const FETCH_TICKETS = base + 'service-board/fetch-all';
+export const GENERATE_TICKETS = base + 'service-board/generate';
+export const ICONS = base + 'build/images/icons';
